e9eb97eadcf98e94938f003b9dc837b87a8fe5de
Viktor Rocha <viktoorrocha@gmail.com>
2025-04-01 16:40:19 -0400

    api/v1/reviews/

--- app/urls.py
+++ app/urls.py
@@ -16,11 +16,5 @@ urlpatterns = [
 
     path('api/v1/', include('actors.urls')),
 
-
-
-    path('reviews/', ReviewCreateListView.as_view(), name='review-create-list'),
-    path('reviews/<int:pk>/', ReviewRetrieveUpdateDestroyView.as_view(), name='review-detail-view'),
-
-
-
+    path('api/v1/', include('reviews.urls')),
 ]
