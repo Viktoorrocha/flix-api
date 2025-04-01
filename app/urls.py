4fa36f0c4ad61db4f9c1436d2fbd571ce2ec3f53
Viktor Rocha <viktoorrocha@gmail.com>
2025-04-01 16:35:45 -0400

    API/V1/Movies

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,6 @@
 from django.contrib import admin
 from django.urls import path, include
 from actors.views import ActorCreateListView, ActorRetrieveUpdateDestroyView
-from movies.views import MovieCreateListView, MovieRetrieveUpdateDestroyView
 from reviews.views import ReviewCreateListView, ReviewRetrieveUpdateDestroyView
 
 
@@ -13,13 +12,10 @@ urlpatterns = [
 
     path('api/v1/', include('genres.urls')),
 
+    path('api/v1/', include('movies.urls')),
+
     path('actors/', ActorCreateListView.as_view(), name='actor-create-list'),
     path('actors/<int:pk>/', ActorRetrieveUpdateDestroyView.as_view(), name='actor-detail-view'),
-
-
-    path('movies/', MovieCreateListView.as_view(), name='movie-create-list'),
-    path('movies/<int:pk>/', MovieRetrieveUpdateDestroyView.as_view(), name='movie-detail-view'),
-
 
     path('reviews/', ReviewCreateListView.as_view(), name='review-create-list'),
     path('reviews/<int:pk>/', ReviewRetrieveUpdateDestroyView.as_view(), name='review-detail-view'),
